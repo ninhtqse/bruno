48664dca597a5ca214b3db3877aa189d261c7b04
ninhtqse <ninhtqse@gmail.com>
2021-04-25 19:19:56 +0700

    fix

--- src/EloquentBuilderTrait.php
+++ src/EloquentBuilderTrait.php
@@ -250,7 +250,7 @@ trait EloquentBuilderTrait
                 // In operations do not have an operator
                 if (in_array($operator, ['in', 'bt'])) {
                     call_user_func_array([$queryBuilder, $method], [
-                        $databaseField, \json_decode($value,true)
+                        $databaseField, $value
                     ]);
                 } else {
                     call_user_func_array([$queryBuilder, $method], [
